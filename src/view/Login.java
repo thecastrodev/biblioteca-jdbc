d6dda5d03d832bd30ff9e1957bbb253a8a9262d3
Eduardo Castro <tec.eduardocastro@gmail.com>
2023-07-04 13:31:21 -0300

    feat: tela login funcional e opcoes no terminal

--- src/view/Login.java
+++ src/view/Login.java
@@ -28,7 +28,7 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					Login frame = new Login();
-					frame.setLocationRelativeTo(null);				
+					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -49,56 +49,68 @@ public class Login extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel_inicio = new JPanel();
 		panel_inicio.setBackground(new Color(192, 192, 192));
 		panel_inicio.setBounds(91, 11, 260, 239);
 		contentPane.add(panel_inicio);
 		panel_inicio.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Usuário");
 		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 11));
 		lblNewLabel.setBounds(38, 44, 53, 20);
 		panel_inicio.add(lblNewLabel);
-		
+
 		textField_usuario = new JTextField();
 		textField_usuario.setBounds(38, 62, 190, 25);
 		panel_inicio.add(textField_usuario);
 		textField_usuario.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Senha");
 		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 11));
 		lblNewLabel_1.setBounds(38, 108, 46, 14);
 		panel_inicio.add(lblNewLabel_1);
-		
+
 		textField_senha = new JTextField();
 		textField_senha.setBounds(38, 123, 190, 25);
 		panel_inicio.add(textField_senha);
 		textField_senha.setColumns(10);
-		
+
 		JButton btnNewButton_login = new JButton("Login");
 		btnNewButton_login.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (textField_usuario.getText()!=null && 
-						!textField_usuario.getText().isEmpty() && 
-						textField_senha.getText()!=null &&
-						!textField_senha.getText().isEmpty()) {
-					JOptionPane.showMessageDialog(btnNewButton_login,"Login feito com sucesso!");
-					dispose();
-					Opcoes Opcoes = new Opcoes();
-					Opcoes.setLocationRelativeTo(null);
-					Opcoes.setVisible(true);
-					}else {
-						JOptionPane.showMessageDialog(btnNewButton_login,"Usuario ou senha incorretos!","Aviso", JOptionPane.WARNING_MESSAGE);
+				String usuario = textField_usuario.getText();
+				String senha = textField_senha.getText();
+				
+				System.out.println(usuario);
+				System.out.println(senha);
+
+				if (usuario != null && !usuario.isEmpty() && senha != null && !senha.isEmpty()) {
+
+					if (usuario.equals("Admin") && senha.equals("Root")) {
+						JOptionPane.showMessageDialog(btnNewButton_login, "Login feito com sucesso!");
+						dispose();
+						Opcoes Opcoes = new Opcoes();
+						Opcoes.setLocationRelativeTo(null);
+						Opcoes.setVisible(true);
+					} else {
+						JOptionPane.showMessageDialog(btnNewButton_login, "Usuario nao cadastrado", "Aviso",
+								JOptionPane.WARNING_MESSAGE);
 					}
+
+				} else {
+					JOptionPane.showMessageDialog(btnNewButton_login, "Algum dos campos esta incorreto ou vazio!", "Aviso",
+							JOptionPane.WARNING_MESSAGE);
+				}
 			}
 		});
+		
 		btnNewButton_login.setForeground(new Color(0, 0, 0));
 		btnNewButton_login.setBackground(new Color(64, 128, 128));
 		btnNewButton_login.setFont(new Font("Verdana", Font.PLAIN, 11));
 		btnNewButton_login.setBounds(73, 159, 113, 30);
 		panel_inicio.add(btnNewButton_login);
-		
+
 		JButton btnNewButton_cadastro = new JButton("Cadastre-se");
 		btnNewButton_cadastro.setForeground(new Color(0, 0, 0));
 		btnNewButton_cadastro.setBackground(new Color(64, 128, 128));
--- src/view/Opcoes.java
+++ src/view/Opcoes.java
@@ -1,22 +1,21 @@
 package view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Color;
-import java.awt.Font;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import model.dao.DaoFactory;
+import model.dao.LivroDao;
+import model.entities.Livro;
 
 public class Opcoes extends JFrame {
 
@@ -30,7 +29,7 @@ public class Opcoes extends JFrame {
 			public void run() {
 				try {
 					Opcoes frame = new Opcoes();
-					frame.setLocationRelativeTo(null);				
+					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -50,36 +49,36 @@ public class Opcoes extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(64, 128, 128));
 		panel.setBounds(0, 0, 384, 261);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JButton btnNewButton_consultar = new JButton("Consultar Livros");
 		btnNewButton_consultar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//realizarConsulta();
+				 realizarConsulta();
 			}
 		});
 		btnNewButton_consultar.setFont(new Font("Verdana", Font.PLAIN, 11));
 		btnNewButton_consultar.setBounds(109, 67, 161, 35);
 		panel.add(btnNewButton_consultar);
-		
+
 		JButton btnNewButton_deslogar = new JButton("Sair");
 		btnNewButton_deslogar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
-			Login Login = new Login();
-			Login.setLocationRelativeTo(null);
-			Login.setVisible(true);
+				Login Login = new Login();
+				Login.setLocationRelativeTo(null);
+				Login.setVisible(true);
 			}
 		});
 		btnNewButton_deslogar.setFont(new Font("Verdana", Font.PLAIN, 11));
 		btnNewButton_deslogar.setBounds(10, 227, 89, 23);
 		panel.add(btnNewButton_deslogar);
-		
+
 		JButton btnNewButton_minhas_reservas = new JButton("Minhas Reservas");
 		btnNewButton_minhas_reservas.setFont(new Font("Verdana", Font.PLAIN, 11));
 		btnNewButton_minhas_reservas.addActionListener(new ActionListener() {
@@ -89,35 +88,48 @@ public class Opcoes extends JFrame {
 		btnNewButton_minhas_reservas.setBounds(109, 134, 161, 35);
 		panel.add(btnNewButton_minhas_reservas);
 	}
-	
-  /*  private void realizarConsulta() {
-        Connection connection = PROPERTIES;
-        if (connection != null) {
-            try {
-                String consulta = "SELECT * FROM equipe494627.livro";
-                PreparedStatement statement = connection.prepareStatement(consulta);
-                ResultSet resultado = statement.executeQuery();
 
-                while (resultado.next()) {
-                    // Processar os dados do resultado da consulta
-                    // Exemplo: String valor = resultado.getString("coluna");
-                }
+	private void realizarConsulta() {
 
-                resultado.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else {
-            JOptionPane.showMessageDialog(null, "Falha na conexão com o banco de dados.","Aviso", JOptionPane.WARNING_MESSAGE);
-        }
-    }
-    
-    public static void main1(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Opcoes Opcoes = new Opcoes();
-            Opcoes.setVisible(true);
-        });
-    }*/
+		LivroDao livroDao = DaoFactory.createLivroDao();
+		List<Livro> list = livroDao.findAll();
+		for (Livro obj : list) {
+			System.out.println(""
+					+ "ISBN: " + obj.getISBN() + "\n"
+					+ "Titulo: " + obj.getTitulo() + "\n"
+					+ "Editora: " + obj.getEditora().getNome() + "\n"
+					+ "Categoria: " + obj.getCategoria().getNome() + "\n"
+					+ "Ano de Lancamento: " + obj.getAnoDeLancamento() + "\n"
+							+ "------------------------------------------------------");
+		}
+
+//		if (connection != null) {
+//			try {
+//				String consulta = "SELECT * FROM Livro";
+//				PreparedStatement statement = connection.prepareStatement(consulta);
+//				ResultSet resultado = statement.executeQuery();
+//
+//				while (resultado.next()) {
+//					// Processar os dados do resultado da consulta
+//					// Exemplo: String valor = resultado.getString("coluna");
+//				}
+//
+//				resultado.close();
+//				statement.close();
+//				connection.close();
+//			} catch (SQLException e) {
+//				throw new DbException(e.getMessage());
+//			}
+//		} else {
+//			JOptionPane.showMessageDialog(null, "Falha na conexão com o banco de dados.", "Aviso",
+//					JOptionPane.WARNING_MESSAGE);
+//		}
+	}
+
+	public static void main1(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			Opcoes Opcoes = new Opcoes();
+			Opcoes.setVisible(true);
+		});
+	}
 }
